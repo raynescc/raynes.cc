a73a8e6956d36e1b145a75f7463a09036a64d397
ajukco <hello@ajuk.co>
2017-11-30 23:54:26 +0000

    Unicornsgit add --all

---
+++ main.js
@@ -0,0 +1,11 @@
+const pressed = [];
+const secretCode = 'andrew';
+window.addEventListener('keyup', (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) {
+    console.log('Woooo you hacked my site!!');
+    cornify_add();
+  }
+});
