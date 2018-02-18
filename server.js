c98377756f6bc10622ae1fa2f08860750993f126
Andrew Raynes <hello@raynes.io>
2018-02-18 23:01:27 +0000

    Took the trash out to the bin. Went shopping

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.use('/', express.static(__dirname));
-app.listen(3000, function() {
-	console.log('Go to http://localhost:3000/index.html')
-});
