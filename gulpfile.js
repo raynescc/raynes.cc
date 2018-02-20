fc049277ad82cbcb9de71d6564ca571e89e663ac
Andrew Raynes <hello@raynes.io>
2018-02-20 22:24:23 +0000

    Improved Gulp build + started Nav Menu

--- gulpfile.js
+++ gulpfile.js
@@ -11,40 +11,47 @@ const gls = require('gulp-live-server');
 
 // Minify all .html files and move to dist
 gulp.task('html', () => {
-gulp.src('./src/*.html')
-.pipe(htmlmin ({ collapseWhitespace: true }))
-.pipe(gulp.dest('./dist/'));
+  gulp.src('./src/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('./dist/'));
 });
 
 // Minify all .js files and move to dist
 gulp.task('js', (cb) => {
-pump ([
-gulp.src('./src/assets/js/*.js'),
-uglify(),
-gulp.dest('./dist/js')
+  pump([
+    gulp.src('./src/assets/js/*.js'),
+    uglify(),
+    gulp.dest('./dist/js')
   ], cb)
 });
 
 // Compile SASS to CSS and minify
 gulp.task('sass', () => {
   gulp.src('./src/assets/scss/*.scss')
-  .pipe(sass())
-  .pipe(uglifycss({
-    "maxLineLen": 80,
-    "uglyComments": true
-  }))
-  .pipe(gulp.dest('./dist/css'))
+    .pipe(sass())
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./dist/css'))
 });
 
 // Server
-  gulp.task('server', () => {
+gulp.task('server', () => {
   const server = gls.static('./dist', 3000);
   server.start();
   gulp.watch(['./dist/css/*.css', './dist/js/*.js', './dist/index.html'], (file) => {
     server.notify.apply(server, [file]);
     console.log('Running on localhost:3000')
   });
-  });
+});
 
-// An array of tasks, allowing you to simply run 'gulp' without the task name
-gulp.task('default', ['html', 'js', 'sass', 'server']);
+// Watch task to watch specific folders for changes
+gulp.task('watch', () => {
+  gulp.watch(['./src/assets/scss/*.scss', './src/assets/js/*.js', './src/*.html'], ['sass', 'js', 'html']);
+});
+
+// An array of tasks, allowing you to simply run 'gulp' for all tasks
+gulp.task('default', ['html', 'js', 'sass', 'server', 'watch']);
