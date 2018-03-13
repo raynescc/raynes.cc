71c095cf2ec0feffe6c98b5d3427911f0a12ffc9
Andrew Raynes <hello@raynes.io>
2018-03-13 20:12:43 +0000

    Added the active class JS for Navbar

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,1 @@
-function showMenu(){var e=document.getElementById("nav");"none"===e.style.display?e.style.display="block":e.style.display="none"}var nodelist=document.getElementsByClassName("insta-img").length;console.log(nodelist);
+function showMenu(){var e=document.getElementById("nav");"none"===e.style.display?e.style.display="block":e.style.display="none"}$(document).ready(function(){var e=window.location.pathname;$('#menu > li > a[href="'+e+'"]').parent().addClass("active")});var nodelist=document.getElementsByClassName("insta-img").length;console.log(nodelist);
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,6 @@
 //TODO: MAKE NAVBAR ANIMATED ON TOGGLE.
 
-
-/* Toggle Navbar (Mobile View) */
+/* toggle navbar  */
 function showMenu() {
   var menu = document.getElementById("nav");
   if (menu.style.display === "none") {
@@ -11,9 +10,20 @@ function showMenu() {
   }
 }
 
+/* add 'active' class to selected navbar item */
+$(document).ready(function() {
+  var pathname = window.location.pathname;
+  $('#menu > li > a[href="' + pathname + '"]').parent().addClass('active');
+})
+
+
+
+
+/* Fix the below */
+
 /* Insta Image Count */
 // function myFunction() {
-    var nodelist = document.getElementsByClassName("insta-img").length;
-    console.log(nodelist);
-    // document.getElementById("demo").innerHTML = nodelist;
+var nodelist = document.getElementsByClassName("insta-img").length;
+console.log(nodelist);
+// document.getElementById("demo").innerHTML = nodelist;
 // }
