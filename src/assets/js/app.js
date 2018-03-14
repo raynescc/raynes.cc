af546101e275ff4e7c8f2a6e712a877b3b7515a0
Andrew Raynes <hello@raynes.io>
2018-03-14 19:59:03 +0000

    Header + footer templates

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,11 @@
 //TODO: MAKE NAVBAR ANIMATED ON TOGGLE.
 
+/* load header/footer templates */
+$(function(){
+  $("#nav").load("nav.html");
+  $("#footer").load("footer.html");
+});
+
 /* toggle navbar  */
 function showMenu() {
   var menu = document.getElementById("nav");
@@ -14,8 +20,13 @@ $(document).ready(function() {
   /* 'active' class navbar */
   var pathname = window.location.pathname;
   $('#menu > li > a[href="' + pathname + '"]').parent().addClass('active');
+
   /* insta photos counts */
   var instaCount = $('.photo').length
   $('.posts').text(instaCount);
-  console.log(instaCount);
+
+  /* update copyright date */
+  var today = new Date()
+  var year = today.getFullYear()
+  $('.copyright').text(year);
 })
