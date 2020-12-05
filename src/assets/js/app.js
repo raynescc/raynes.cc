7b76f6b06707a647bc6e278345e11f1b4b4d5595
Andrew <andrew@raynes.io>
2020-12-05 20:18:20 +0000

    Updated text for personal site

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,4 @@
-/* Toggle navbar  */
+// Toggle navbar
 function showMenu() {
   var menu = document.getElementById("nav");
   if (menu.style.display === "none") {
@@ -8,28 +8,28 @@ function showMenu() {
   }
 }
 
-/* Document.ready */
-$(document).ready(function() {
-
+$(document).ready(function () {
   // Fade page in on load
-  $(".fade").hide(0).delay(0).fadeIn(5000)
+  $(".fade").hide(0).delay(0).fadeIn(1000);
 
   // 'active' class navbar
   var pathname = window.location.pathname;
-  $('#menu > li > a[href="' + pathname + '"]').parent().addClass('active');
+  $('#menu > li > a[href="' + pathname + '"]')
+    .parent()
+    .addClass("active");
 
   // Age
-  var ageElement = document.getElementById('age');
+  var ageElement = document.getElementById("age");
   if (ageElement) {
-    var start = new Date('09/20/1991 06:00 AM');
+    var start = new Date("09/20/1991 06:00 AM");
     var timer;
 
     function age() {
       var now = new Date();
       var age = now.getTime() - start.getTime();
-      var year = (age / 31556926000);
+      var year = age / 31556926000;
       ageElement.innerHTML = year.toFixed(8);
     }
     timer = setInterval(age, 1);
   }
-})
+});
